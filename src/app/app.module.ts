e4dd596f1e165f28eb0afd9efeaff01cd793b421
PatriciaContreras <patricia.contreras-carranque@capgemini.com>
2023-05-18 17:28:06 +0200

    resolucion conflicto dependencias implementado multiidioma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,47 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable, catchError, switchMap, throwError } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+
+class CustomLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<any> {
+    const language = lang;
+
+    let url = `./assets/i18n/${language}.json`;
+
+    return fromFetch(url, { method: 'GET' }).pipe(
+      switchMap((response: any) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw response;
+        }
+      }),
+      catchError((err) => {
+        console.error(`No se ha podido obtener el fichero de traducción para el idioma "${language}"`);
+        return throwError(() => err);
+      })
+    );
+  }
+}
+
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,  TranslateModule.forRoot({
+    loader: { provide: TranslateLoader, useClass: CustomLoader },
+    defaultLanguage: 'es'
+  })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private translate: TranslateService){}
+
+ handleChange = ($event: any) => {
+  this.translate.use($event.detail.value);
+}
 }
