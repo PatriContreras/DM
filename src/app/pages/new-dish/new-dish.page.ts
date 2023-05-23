e471f6fd7d3631aff3261e34da39a34358958304
PatriciaContreras <patricia.contreras-carranque@capgemini.com>
2023-05-23 18:13:27 +0200

    mejora de layout y estilos

--- src/app/pages/new-dish/new-dish.page.ts
+++ src/app/pages/new-dish/new-dish.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-new-dish',
@@ -7,27 +10,65 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./new-dish.page.scss'],
 })
 export class NewDishPage implements OnInit {
+  // public get alertCtrl(): AlertController {
+  //   return this._alertCtrl;
+  // }
+  // public set alertCtrl(value: AlertController) {
+  //   this._alertCtrl = value;
+  // }
 
   ingredientsList: string[] = [];
-
   form!: FormGroup<any>;
-
-  constructor() { }
+  btnCancel: string = '';
+  constructor(private _alertCtrl: AlertController, private router: Router, private _translate: TranslateService) { }
 
   ngOnInit() {
     this.form = new FormGroup({
-      name: new FormControl('', [Validators.required]),
-      schedule: new FormControl('',  [Validators.required]),
-      ingredient: new FormControl('',  [Validators.required]),
+      name: new FormControl(''),
+      schedule: new FormControl(''),
+      ingredient: new FormControl(''),
     })
   }
 
-  addIngredient(){
+  addIngredient() {
     this.ingredientsList.push(this.form.value.ingredient)
   }
 
-  deleteIngredient(ingredient: string){
+  deleteIngredient(ingredient: string) {
+    const index = this.ingredientsList.indexOf(ingredient);
+    this.ingredientsList.splice(index, 1);
+  }
 
+  async onSubmit() {
+    this.showAlert();
+    //TODO Save this.form.value in database
+  }
+
+  private async showAlert() {
+    let alert = await this._alertCtrl.create({
+      message: this._translate.instant('new.alert.message'),
+      buttons: [
+        {
+          text: this._translate.instant('new.alert.btn1'),
+          handler: () => {
+            this.btnCancel = 'new';
+            this.form.reset();
+            this.ingredientsList = [];
+            this.router.navigate(['/new'])
+          }
+        },
+        {
+          text: this._translate.instant('new.alert.btn2'),
+          handler: () => {
+            this.btnCancel = 'home';
+            this.form.reset();
+            this.ingredientsList = [];
+            this.router.navigate(['/home'])
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
